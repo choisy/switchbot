f46da18213ef8e9e4fa52dd58f6fd2c1adac3d3b
Marc Choisy <choisy@users.noreply.github.com>
2026-06-30 16:11:41 +0200

    Code cleaning + data update

--- reading data.R
+++ reading data.R
@@ -1,9 +1,11 @@
 library(dplyr)
 
+
 root <- paste0("/Users/", Sys.getenv("USER"),
                "/Library/CloudStorage/Dropbox/SwitchBot/")
 data_raw <- paste0(root, "data_raw/")
 data_clean <- paste0(root, "data_clean/")
+
 
 get_time <- function(data, folder) {
   data |> 
@@ -12,6 +14,7 @@ get_time <- function(data, folder) {
     paste(folder, ":", .)
 }
 
+
 common_code <- function(x, slot_names) {
   x |> 
     setNames(slot_names) |> 
@@ -19,6 +22,7 @@ common_code <- function(x, slot_names) {
     bind_rows(.id = "file") |> 
     mutate(across(Timestamp, lubridate::mdy_hms))
 } 
+
 
 load_data <- function(folder) {
   folder_name <- paste0(data_raw, folder)
@@ -34,10 +38,6 @@ load_data <- function(folder) {
     if (length(slot_names)) {
       out <- slot_names %>% 
         paste0(folder_name, "/", .) |> 
-#        setNames(slot_names) |> 
-#        purrr::map(readr::read_csv) |> 
-#        bind_rows(.id = "file") |> 
-#        mutate(across(Timestamp, lubridate::mdy_hms)) |> 
         common_code(slot_names) |> 
         bind_rows(tmp)
     } else {
@@ -47,62 +47,19 @@ load_data <- function(folder) {
     slot_names <- dir(folder_name)
     out <- folder_name |> 
       dir(full = TRUE) |> 
-#      setNames(slot_names) |> 
-#      purrr::map(readr::read_csv) |> 
-#      bind_rows(.id = "file") |> 
-#      mutate(across(Timestamp, lubridate::mdy_hms))
       common_code(slot_names)
   }
-  out |> 
+  out <- out |> 
     arrange(Timestamp) |> 
-    unique() |> 
-    saveRDS(clean_file)
+    unique()
+  saveRDS(out, clean_file)
   get_time(out, folder)
 }
 
 
-#data_raw |> 
-#  dir() |> 
-c("etage", "jardin" ) |> 
+data_raw |> 
+  dir() |> 
   purrr::map_chr(load_data) |> 
   cat(file = "README.md", sep = "\n\n")
 
-#folder <- "jardin"
-
-
-#####################################
-
-#load_data("jardin")
-#tmp <- readRDS("jardin.rds")
-#
-#tmp |> 
-#  pull(file) |> 
-#  unique()
-#
-#  
-#  paste0(root, folder) |>
-#    dir(full = TRUE) |> 
-#    purrr::map(readr::read_csv) |> 
-#    bind_rows(.id = "file") |> 
-#    mutate(across(Timestamp, lubridate::mdy_hms)) |> 
-#    arrange(Timestamp) |> 
-#    unique() |> 
-#    saveRDS(paste0(folder, ".rds"))
-#}
-#
-#walk(root, dir)
-#
-#load_data("jardin")
-#load_data("etage")
-#
-#readRDS("jardin.rds")
-#
-#
-#dir(root)
-#
-#write(format(tail(a$Timestamp, 1)), "README.md")
-#
-#write(format(tail(sbdata$Timestamp, 1)), "README.md")
-#
-##with(a, plot(Timestamp, `Temperature_Celsius(°C)`, type = "l", col = 4))
-#
+## with(a, plot(Timestamp, `Temperature_Celsius(°C)`, type = "l", col = 4))
